638ee7d499abf20e2cb16143ac7f80097263f13c
Kübra Varol <kubravarol499@gmail.com>
2025-07-21 21:54:03 +0300

    Add ConsumerConfig

--- src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
@@ -16,7 +16,9 @@ public class KafkaDemoApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
 		return args -> {
-			kafkaTemplate.send("Kafka-demo", "Hello Kafka");
+			for(int i=0; i<100; i++) {
+				kafkaTemplate.send("Kafka-demo", i + " Hello Kafka :) ");
+			}
 		};
 	}
 }
